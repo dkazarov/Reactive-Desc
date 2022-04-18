b9e55898a7cd5445854fde99652a0510f5fcb35f
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-04-19 01:15:58 +0300

    add normalize & spinner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import TableContacts from './components/Table/TableContacts';
+import Loader from './components/Loader/Loader';
 
 import axios from 'axios';
 
@@ -8,17 +9,19 @@ import './App.scss';
 
 function App() {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const _baseUrl =
     'http://www.filltext.com/?rows=32&id={...|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
 
   useEffect(() => {
     axios.get(_baseUrl).then((response) => setData(response.data));
+    setIsLoading(true);
   }, []);
 
   return (
     <div className="container">
-      <TableContacts data={data} />
+      {isLoading ? <Loader /> : <TableContacts data={data} />}
     </div>
   );
 }
