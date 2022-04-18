96630b405074432748cf463a744bf52177f13fe8
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-04-19 00:50:34 +0300

    get data from server

--- src/App.js
+++ src/App.js
@@ -1,7 +1,56 @@
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
+import Table from 'react-bootstrap/Table';
+
 import './App.scss';
 
+const _baseUrl =
+  'http://www.filltext.com/?rows=32&id={...|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
+
 function App() {
-  return <><h1>111</h1></>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(_baseUrl).then((response) => setData(response.data));
+  }, []);
+
+  console.log(data);
+
+  return (
+    <div className="container">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan={2}>Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 
 export default App;
