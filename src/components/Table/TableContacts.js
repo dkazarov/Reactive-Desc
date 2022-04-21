83c80e75ec2d144b214143e7446c1263d3d8deba
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-04-21 20:04:44 +0300

    add detail user modal window

--- src/components/Table/TableContacts.js
+++ src/components/Table/TableContacts.js
@@ -3,11 +3,15 @@ import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 
 import Table from 'react-bootstrap/Table';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
 
 import { SortIconDown, SortIconUp } from './SortIcon/SortIcon';
 
 const TableContacts = ({ data, setData, sortDirection, setSortDirection }) => {
   const [fieldData, setFieldData] = useState('');
+  const [modalShow, setModalShow] = React.useState(false);
+  const [userInfo, setUserInfo] = useState('');
   //Sort Columns
   const sortData = (field) => {
     // Sort Data
@@ -29,6 +33,49 @@ const TableContacts = ({ data, setData, sortDirection, setSortDirection }) => {
     sortData(field);
     setFieldData(field);
     console.log(field);
+  };
+
+  function MyVerticallyCenteredModal(props) {
+    console.log(props);
+    return (
+      <Modal
+        {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Користувач: {props.userinfo.firstName} {props.userinfo.lastName}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h2>
+            Ім'я: {props.userinfo.firstName} {props.userinfo.lastName}
+          </h2>
+          <h5>
+            <address>
+              Країна: {props.userinfo.address.state}, Місто:{' '}
+              {props.userinfo.address.city}, Адреса:{' '}
+              {props.userinfo.address.address}
+            </address>
+            <hr />
+              <h4>Вік: {props.userinfo.age} ріків</h4>
+          </h5>
+
+          <h5>Єлектронна пошта: {props.userinfo.email}</h5>
+          <h5>Телефон: {props.userinfo.phone}</h5>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={props.onHide}>Close</Button>
+        </Modal.Footer>
+      </Modal>
+    );
+  }
+
+  const detailModal = (items) => {
+    setModalShow(true);
+    setUserInfo(items);
   };
 
   return (
@@ -63,7 +110,7 @@ const TableContacts = ({ data, setData, sortDirection, setSortDirection }) => {
       </thead>
       <tbody xs={6}>
         {data.map((items) => (
-          <tr key={nanoid()}>
+          <tr key={nanoid()} onClick={() => detailModal(items)}>
             <td>{items.id}</td>
             <td>{items.firstName}</td>
             <td>{items.lastName}</td>
@@ -73,6 +120,11 @@ const TableContacts = ({ data, setData, sortDirection, setSortDirection }) => {
           </tr>
         ))}
       </tbody>
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+        userinfo={userInfo}
+      />
     </Table>
   );
 };
