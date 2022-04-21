d7105722c9442135482bc7572761b493cf91c0bb
Dmitry Kazarov <d.kazarov1361@gmail.com>
2022-04-22 01:33:27 +0300

    fix

--- src/components/Table/SortIcon/SortIcon.js
+++ src/components/Table/SortIcon/SortIcon.js
@@ -18,7 +18,7 @@ export const SortIconDown = () => {
 
 export const SortIconUp = () => {
   return (
-    <span style={{ marginLeft: '5px', display: 'inline-block' }}>
+    <span style={{ marginLeft: '5px', display: 'inline' }}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="16"
